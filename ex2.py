5fd7f3ea6d664b78bfeb3e68de6e933fa857e39f
Gaetan <150467427+GaetanAL@users.noreply.github.com>
2024-09-11 17:08:33 -0400

    bon fonctionnement exo4

--- ex2.py
+++ ex2.py
@@ -1,4 +1,17 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
 
-water_quantity = ...
+water_quantity = float(input("Quelle quantité d'eau faut-il assainir ? "))
+filter_quantity = water_quantity/5
+lamp_quantity = water_quantity*(3/5)
+chlore_amount = water_quantity*(0.5/5)
+
+
+
+print(f"Voici les éléments requis pour assainir {water_quantity}L d'eau:\n"
+       f"         - Filtres : {filter_quantity}\n"
+       f"         - Lampes UV : {lamp_quantity}\n"
+       f"         - Chlore : {chlore_amount}kg"
+       )
+
+
