a340f8af8dfc2c7928bc771fd75b9f8389246bdd
Jad Charbachi <157416330+JadCharbachi@users.noreply.github.com>
2024-09-11 17:27:38 -0400

    exo1,3,4
    exo1,3,4

--- ex3.py
+++ ex3.py
@@ -1,5 +1,11 @@
 # TODO: Calculer la distance qu'un poids peut atteindre en utilisant la formule de la portée d'un projectile.
 # TODO: Importer les modules nécessaires.
 
-speed = ... 
-angle = ...
+from math import sin
+from math import radians
+
+speed = input("Vitesse initiale (m/s) : ")
+angle = input("Angle de lancer (en degrés) : ")
+distance = round((float(speed)**2 * sin(2*radians(float(angle))) / 9.81), 2)
+print(f"Distance parcourure : {distance} m")
+
--- ex4.py
+++ ex4.py
@@ -2,5 +2,21 @@
 #        calcule la distance restante en km en fonction de ce pourcentage,
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
-
-battery_level = ...
+battery_level = input("Pourcentage de batterie? ")
+while (battery_level.isdigit() == False or int(battery_level) > 100 or int(battery_level) < 0):
+    battery_level = input("Veuillez insérer un pourcentage de batterie valide (entre 0 et 100): ")
+battery_level = int(battery_level)
+distance = battery_level
+if(battery_level > 50):
+    distance = distance * 2
+elif(battery_level > 25):
+    distance*= 0.5
+elif(battery_level > 10):
+    distance*= 1
+elif(battery_level > 5):
+    distance*= 2.5
+else:
+    distance*= 6
+    
+distance = round(distance,1)
+print(distance, "km")
