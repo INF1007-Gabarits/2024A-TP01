d8c0a62cb34111f321b129343f21bafc6cf8f993
Gaetan <150467427+GaetanAL@users.noreply.github.com>
2024-09-11 17:27:09 -0400

    bon fonctionnement exo5

--- ex5.py
+++ ex5.py
@@ -1,5 +1,16 @@
 #TODO: Analyser la chaîne de caractères saisie et compter le nombre de médailles.
 #      Attention si la chaîne est invalide, un message d'erreur est attendu.
 
-country = ...
-code_medals = ...
+country = input("Pays concerné ? ")
+code_medals = input("Chaine représentant les médailles ? ")
+for c in code_medals:
+    gold_medals = code_medals.count("G");
+    silver_medals = code_medals.count("S");
+    bronze_medals = code_medals.count("B");
+
+
+print(f"{country}:\n"
+      f"- {gold_medals} OR\n"
+      f"- {silver_medals} Argent\n"
+      f"- {bronze_medals} Bronze\n"
+      )
